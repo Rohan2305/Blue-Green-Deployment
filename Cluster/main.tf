6c56a0600242876f9457a73ed7f49625870f256b
Rohan Madane <152596448+Rohan2305@users.noreply.github.com>
2025-01-19 19:36:19 +0530

    Update main.tf

--- Cluster/main.tf
+++ Cluster/main.tf
@@ -116,7 +116,7 @@ resource "aws_eks_node_group" "devopsshack" {
   }
 }
 
-resource "aws_iam_role" "devopsshack_cluster_role_1" {
+resource "aws_iam_role" "devopsshack_cluster_role" {
   name = "devopsshack-cluster-role"
 
   assume_role_policy = <<EOF
@@ -140,7 +140,7 @@ resource "aws_iam_role_policy_attachment" "devopsshack_cluster_role_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
-resource "aws_iam_role" "devopsshack_node_group_role_1" {
+resource "aws_iam_role" "devopsshack_node_group_role" {
   name = "devopsshack-node-group-role"
 
   assume_role_policy = <<EOF
